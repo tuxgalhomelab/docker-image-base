99e891afb8c191e4cc5e321b564137229af7b093
G3 <tuxgal@Baymax.area51>
2022-02-01 23:17:33 -0800

    Change the output of build-args.sh to be github workflow friendly.

--- build-args.sh
+++ build-args.sh
@@ -5,34 +5,36 @@ args_file_as_build_args() {
     local prefix=""
     if [[ "$1" == "docker-flags" ]]; then
         prefix="--build-arg "
+        while IFS="=" read -r key value; do
+            echo -n "${prefix}$key=\"$value\" "
+        done < "args"
+    else
+        while IFS="=" read -r key value; do
+            echo "$key=$value"
+        done < "args"
     fi
-    while IFS="=" read -r key value; do
-        echo -n "${prefix}$key=\"$value\" "
-    done < "args"
 }
 
 packages_to_install_file_as_build_arg() {
-    local prefix=""
-    if [[ "$1" == "docker-flags" ]]; then
-        prefix="--build-arg "
-    fi
-    echo -n "${prefix}PACKAGES_TO_INSTALL=\""
     while IFS="=" read -r key value; do
         echo -n "$key=$value "
     done < "packages-to-install"
-    echo -n "\""
 }
 
 packages_to_remove_file_as_build_arg() {
-    local prefix=""
-    if [[ "$1" == "docker-flags" ]]; then
-        prefix="--build-arg "
-    fi
-    echo -n "${prefix}PACKAGES_TO_REMOVE=\""
     while IFS="=" read -r key; do
         echo -n "$key "
     done < "packages-to-remove"
-    echo -n "\""
 }
 
-echo "$(args_file_as_build_args $1)$(packages_to_install_file_as_build_arg $1) $(packages_to_remove_file_as_build_arg $1)"
+if [[ "$1" == "docker-flags" ]]; then
+    # --build-arg format used with the docker build command.
+    args_file_as_build_args $1
+    echo -n "--build-arg PACKAGES_TO_INSTALL=\"$(packages_to_install_file_as_build_arg)\" "
+    echo -n "--build-arg PACKAGES_TO_REMOVE=\"$(packages_to_remove_file_as_build_arg)\""
+else
+    # Github Env format dump.
+    args_file_as_build_args
+    echo "PACKAGES_TO_INSTALL=$(packages_to_install_file_as_build_arg)"
+    echo "PACKAGES_TO_REMOVE=$(packages_to_remove_file_as_build_arg)"
+fi
